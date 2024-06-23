fe573c3abbd9535358b18a39edb7eb0c9546f96e
wilfred130 <kisituwilfred130@gmail.com>
2024-06-23 13:11:19 -0700

    db configuration changes

--- configs/db_cofigs.js
+++ configs/db_cofigs.js
@@ -3,9 +3,6 @@ exports.dbConfigs = {
         database: `wilfred`,
         username: 'wilfred',
         password: 'wilfred',
-<<<<<<< HEAD
-=======
->>>>>>> e359c9254861661238258be4ad1b6440d0790151
         host:'localhost',
         dialect: `mysql`,
     }
